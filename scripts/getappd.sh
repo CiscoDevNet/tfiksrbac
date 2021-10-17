27c0d113c1790997a89409b0a6e94351194aeea1
prathjan <prathjan@cisco.com>
2021-10-16 20:44:46 -0700

    comments

--- scripts/getappd.sh
+++ scripts/getappd.sh
@@ -10,28 +10,28 @@ function parse_input() {
   if [[ -z "${NBRNET}" ]]; then export NBRNET=none; fi
 }
 
-parse_input
-echo "syarting"
-apt install dos2unix -y
-rm -rf /home/ec2-user
-mkdir -p /home/ec2-user/environment/workshop/
-cp /tmp/devnet-controller-setup.zip /home/ec2-user/environment/workshop
-cd /home/ec2-user/environment/workshop
-unzip /home/ec2-user/environment/workshop/devnet-controller-setup.zip -d /home/ec2-user/environment/workshop
-chmod +x /home/ec2-user/environment/workshop/*.sh
-sed 's/nbrapm/'${NBRAPM}'/g' /tmp/workshop-setup.yaml > /tmp/workshop.file
-sed 's/nbrma/'${NBRMA}'/g' /tmp/workshop.file > /tmp/workshop1.file
-sed 's/nbrsim/'${NBRSIM}'/g' /tmp/workshop1.file > /tmp/workshop2.file
-sed 's/nbrnet/'${NBRNET}'/g' /tmp/workshop2.file > /tmp/workshop3.file
-cp /tmp/workshop3.file /home/ec2-user/environment/workshop/workshop-setup.yaml
-export appd_workshop_user=SBUser
-/home/ec2-user/environment/workshop/setupWorkshop.sh
-dos2unix /home/ec2-user/environment/workshop/application.env
-. /home/ec2-user/environment/workshop/application.env
-echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY > /tmp/accesskey
+#parse_input
+#echo "syarting"
+#apt install dos2unix -y
+#rm -rf /home/ec2-user
+#mkdir -p /home/ec2-user/environment/workshop/
+#cp /tmp/devnet-controller-setup.zip /home/ec2-user/environment/workshop
+#cd /home/ec2-user/environment/workshop
+#unzip /home/ec2-user/environment/workshop/devnet-controller-setup.zip -d /home/ec2-user/environment/workshop
+#chmod +x /home/ec2-user/environment/workshop/*.sh
+#sed 's/nbrapm/'${NBRAPM}'/g' /tmp/workshop-setup.yaml > /tmp/workshop.file
+#sed 's/nbrma/'${NBRMA}'/g' /tmp/workshop.file > /tmp/workshop1.file
+#sed 's/nbrsim/'${NBRSIM}'/g' /tmp/workshop1.file > /tmp/workshop2.file
+#sed 's/nbrnet/'${NBRNET}'/g' /tmp/workshop2.file > /tmp/workshop3.file
+#cp /tmp/workshop3.file /home/ec2-user/environment/workshop/workshop-setup.yaml
+#export appd_workshop_user=SBUser
+#/home/ec2-user/environment/workshop/setupWorkshop.sh
+#dos2unix /home/ec2-user/environment/workshop/application.env
+#. /home/ec2-user/environment/workshop/application.env
+#echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY > /tmp/accesskey
 
-access="$(echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)"
-access=testing
+#access="$(echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY)"
+#access=testing
 #echo -n "{\"download\":${download}, \"install\":${install}}" | tr -d ']['
 #echo -n "{\"accesskey\":thisisit}" | tr -d ']['
 #echo -n "{\"download\":${download}, \"install\":${install}}" | tr -d ']['
