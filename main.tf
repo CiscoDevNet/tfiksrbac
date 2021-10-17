5d3af624708198c6b58e4d41d638a3889bfbcefc
prathjan <prathjan@cisco.com>
2021-10-17 13:13:34 -0700

    hard values

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 data "external" "appd" {
   program = ["bash", "./scripts/getappd.sh"]
   query = {
-    nbrapm = "${var.nbrapm}"
-    nbrma = "${var.nbrma}"
-    nbrsim  = "${var.nbrsim}"
-    nbrnet = "${var.nbrnet}"
+    #nbrapm = "${var.nbrapm}"
+    #nbrma = "${var.nbrma}"
+    #nbrsim  = "${var.nbrsim}"
+    #nbrnet = "${var.nbrnet}"
+    nbrapm = 8
+    nbrma = 1
+    nbrsim  = 0
+    nbrnet = 0
   }
 }
 
