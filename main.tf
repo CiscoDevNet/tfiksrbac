2c780e753886122cf1c00686d515186bd7658320
prathjan <prathjan@cisco.com>
2021-10-19 15:57:07 -0700

    moved to k8s Job

--- main.tf
+++ main.tf
@@ -1,34 +1,52 @@
-data "external" "appd" {
-  program = ["bash", "./scripts/getappd.sh"]
-  query = {
-    #nbrapm = "${var.nbrapm}"
-    #nbrma = "${var.nbrma}"
-    #nbrsim  = "${var.nbrsim}"
-    #nbrnet = "${var.nbrnet}"
-    nbrapm = 8
-    nbrma = 1
-    nbrsim  = 0
-    nbrnet = 0
+
+#Helm install of sample app on IKS
+data "terraform_remote_state" "iksws" {
+  backend = "remote"
+  config = {
+    organization = var.org
+    workspaces = {
+      name = var.ikswsname
+    }
   }
 }
 
-
-variable "nbrapm" {
-  type = string
-}
-variable "nbrma" {
-  type = string
-}
-variable "nbrsim" {
-  type = string
-}
-variable "nbrnet" {
-  type = string
+provider "kubernetes" {
+    host = local.kube_config.clusters[0].cluster.server
+    client_certificate = base64decode(local.kube_config.users[0].user.client-certificate-data)
+    client_key = base64decode(local.kube_config.users[0].user.client-key-data)
+    cluster_ca_certificate = base64decode(local.kube_config.clusters[0].cluster.certificate-authority-data)
 }
 
-output "accesskey" {
-  value = data.external.appd.result["accesskey"]
+variable "org" {
+  type = string
+}
+variable "ikswsname" {
+  type = string
+}
+
+resource "kubernetes_namespace" "appd" {
+  metadata {
+    name = "appdynamics"
+  }
+}
+
+resource "kubernetes_config_map" "example" {
+  metadata {
+    name = "my-config"
+    namespace = kubernetes_namespace.appd.name
+  }
+
+  data = {
+    "devnet-controller-setup.zip" = "${file("${path.module}/scripts/devnet-controller-setup.zip")}"
+    "workshop-setup.yaml" = "${file("${path.module}/scripts/workshop-setup.yaml")}"
+    "rbac.sh" = "${file("${path.module}/scripts/rbac.sh")}"
+  }
+
 }
 
 
+locals {
+  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+  kube_config_str = data.terraform_remote_state.iksws.outputs.kube_config
+}
 
