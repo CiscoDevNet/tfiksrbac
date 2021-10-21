ebd4762da476e5dad8e6721432cbbdf218cfe883
prathjan <prathjan@cisco.com>
2021-10-21 09:30:15 -0700

    updates to rbac script

--- scripts/rbac.sh
+++ scripts/rbac.sh
@@ -1,18 +1,23 @@
-apt install dos2unix -y
-rm -rf /home/ec2-user
-mkdir -p /home/ec2-user/environment/workshop/
-cp /tmp/devnet-controller-setup.zip /home/ec2-user/environment/workshop 
-#cd /home/ec2-user/environment/workshop
-unzip /home/ec2-user/environment/workshop/devnet-controller-setup.zip -d /home/ec2-user/environment/workshop
-chmod +x /home/ec2-user/environment/workshop/*.sh
+sudo apt update
+sudo apt install dos2unix -y
+sudo apt install unzip -y
+sudo apt install openjdk-8-jdk -y
+sudo rm -rf /home/ec2-user
+sudo mkdir -p /home/ec2-user/environment/workshop/
+sudo cp /tmp/devnet-controller-setup.zip /home/ec2-user/environment/workshop
+sudo unzip /home/ec2-user/environment/workshop/devnet-controller-setup.zip -d /home/ec2-user/environment/workshop
+sudo chmod +x /home/ec2-user/environment/workshop/setupWorkshop.sh
+sudo chmod +x /home/ec2-user/environment/workshop/teardownWorkshop.sh
+sudo chmod +x /home/ec2-user/environment/workshop/terminateApps.sh
+sudo chmod +x /home/ec2-user/environment/workshop/workshopPrereqs.sh
 sed 's/nbrapm/'$1'/g' /tmp/workshop-setup.yaml > /tmp/workshop.file
 sed 's/nbrma/'$2'/g' /tmp/workshop.file > /tmp/workshop1.file
 sed 's/nbrsim/'$3'/g' /tmp/workshop1.file > /tmp/workshop2.file
 sed 's/nbrnet/'$4'/g' /tmp/workshop2.file > /tmp/workshop3.file
-cp /tmp/workshop3.file /home/ec2-user/environment/workshop/workshop-setup.yaml
+sudo cp /tmp/workshop3.file /home/ec2-user/environment/workshop/workshop-setup.yaml
 export appd_workshop_user=SBUser
-/home/ec2-user/environment/workshop/setupWorkshop.sh
-dos2unix /home/ec2-user/environment/workshop/application.env
-. /home/ec2-user/environment/workshop/application.env
+sudo /home/ec2-user/environment/workshop/setupWorkshop.sh
+#dos2unix /home/ec2-user/environment/workshop/application.env
+sudo source /home/ec2-user/environment/workshop/application.env
 echo $APPDYNAMICS_AGENT_ACCOUNT_ACCESS_KEY > /tmp/accesskey
 
