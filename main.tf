68316f718cb37df82642501f8fcb522a491fd0a6
prathjan <prathjan@cisco.com>
2021-10-20 19:01:52 -0700

    with real

--- main.tf
+++ main.tf
@@ -1,13 +1,26 @@
-
+#Helm install of sample app on IKS
+data "terraform_remote_state" "iksws" {
+  backend = "remote"
+  config = {
+    organization = var.org
+    workspaces = {
+      name = var.ikswsname
+    }
+  }
+}
 data "external" "host" {
   program = ["bash", "./scripts/gethost.sh"]
   query = {
-    url = "https://10.88.168.175:6443"
+    #url = "https://10.88.168.175:6443"
     #url = "http://myweb.com:8080"
+    url = local.kube_config.clusters[0].cluster.server
   }
 }
 
 
+locals {
+  kube_config = yamldecode(data.terraform_remote_state.iksws.outputs.kube_config)
+}
 
 output "host" {
   value = data.external.host.result["host"]
