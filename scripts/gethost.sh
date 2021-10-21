2f7b96c4155e28761658638b68601628d9a73cc7
prathjan <prathjan@cisco.com>
2021-10-20 18:56:39 -0700

    uncommented

--- scripts/gethost.sh
+++ scripts/gethost.sh
@@ -13,20 +13,20 @@ hosturl="${URL}"
 # extract the protocol
 proto="$(echo $hosturl | grep :// | sed -e's,^\(.*://\).*,\1,g')"
 # remove the protocol
-#url="$(echo ${hosturl/$proto/})"
+url="$(echo ${hosturl/$proto/})"
 # extract the user (if any)
-#user="$(echo $url | grep @ | cut -d@ -f1)"
+user="$(echo $url | grep @ | cut -d@ -f1)"
 # extract the host and port
-#hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
+hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
 # by request host without port    
-#host="$(echo $hostport | sed -e 's,:.*,,g')"
+host="$(echo $hostport | sed -e 's,:.*,,g')"
 # by request - try to extract the port
 #port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 # extract the path (if any)
 #path="$(echo $url | grep / | cut -d/ -f2-)"
 
-host="${URL}"
-echo -n "{\"host\":\"${proto}\"}"
+#host="${URL}"
+echo -n "{\"host\":\"${host}\"}"
 #echo -n "{\"host\":\"${URL}\"}"
 #echo -n "{\"host\":\"testing\"}"
 
