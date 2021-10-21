e77c26a03ba78cce13d357891e7e13fd504f6bb3
prathjan <prathjan@cisco.com>
2021-10-20 18:42:09 -0700

    only url

--- scripts/gethost.sh
+++ scripts/gethost.sh
@@ -10,19 +10,19 @@ function parse_input() {
 parse_input()
 
 # extract the protocol
-proto="$(echo ${URL} | grep :// | sed -e's,^\(.*://\).*,\1,g')"
+#proto="$(echo ${URL} | grep :// | sed -e's,^\(.*://\).*,\1,g')"
 # remove the protocol
-url="$(echo ${URL/$proto/})"
+#url="$(echo ${URL/$proto/})"
 # extract the user (if any)
-user="$(echo $url | grep @ | cut -d@ -f1)"
+#user="$(echo $url | grep @ | cut -d@ -f1)"
 # extract the host and port
-hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
+#hostport="$(echo ${url/$user@/} | cut -d/ -f1)"
 # by request host without port    
-host="$(echo $hostport | sed -e 's,:.*,,g')"
+#host="$(echo $hostport | sed -e 's,:.*,,g')"
 # by request - try to extract the port
-port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
+#port="$(echo $hostport | sed -e 's,^.*:,:,g' -e 's,.*:\([0-9]*\).*,\1,g' -e 's,[^0-9],,g')"
 # extract the path (if any)
-path="$(echo $url | grep / | cut -d/ -f2-)"
+#path="$(echo $url | grep / | cut -d/ -f2-)"
 
 #echo -n "{\"host\":\"${host}\"}"
 echo -n "{\"host\":\"${URL}\"}"
